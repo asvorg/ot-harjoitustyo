134276262c3c69761c70640b285e147d9ab857b9
Aapo Koskinen <aapo_koskinen@hotmail.com>
2023-04-18 19:51:59 +0300

    commit

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-
-[gitlog.txt](https://github.com/asvorg/ot-harjoitustyo/blob/master/laskarit/viikko1/gitlog.txt)
-
-[komentorivi.txt](https://github.com/asvorg/ot-harjoitustyo/blob/master/laskarit/viikko1/komentorivi.txt)
-
 [Functional specifications](https://github.com/asvorg/ot-harjoitustyo/blob/master/documentation/vaatimusm%C3%A4%C3%A4rittely.md)
 
 [Hours spent](https://github.com/asvorg/ot-harjoitustyo/blob/master/documentation/tuntikirjanpito.md)
--- tasks.py
+++ tasks.py
@@ -1,9 +1,17 @@
 from invoke import task
 
 @task
-def test(ctx):
-    ctx.run("coverage report -m --omit=/usr/**,salasana-wallet/src/tests/**", pty = True)
-
-@task(test)
 def coverage_report(ctx):
-    ctx.run("coverage html --omit=/usr/**,salasana-wallet/src/tests/**", pty=True)
+    ctx.run("coverage html --omit=/usr/**,salasana-wallet/src/tests/", pty=True)
+
+#@task(coverage)
+#def coverage_report(ctx):
+#    ctx.run("coverage html", pty=True)
+
+@task
+def start(ctx):
+    ctx.run("python3 src/main/main.py", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src")
